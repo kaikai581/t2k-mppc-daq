3a46b15208f607b5b8025d0f7855aee748b43da6
kaikai <kaikai581@hotmail.com>
2020-07-31 18:43:18 -0500

    Analysis work in progress...

--- FEBDAQMULTx2/data_analysis/validation/single_channel_photon_distribution.py
+++ FEBDAQMULTx2/data_analysis/validation/single_channel_photon_distribution.py
@@ -8,8 +8,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_filename', type=str,
            default='mppc_20200728.root')
+    parser.add_argument('-b', '--board_id', type=int, default=0)
+    parser.add_argument('-c', '--channel', type=int, default=0)
     args = parser.parse_args()
     infpn = args.input_filename
+    board_id = args.board_id
+    channel = args.channel
+
+    # read data with uproot
+    tr = uproot.open(infpn)['mppc']
+    df = tr.pandas.df()
+    # add a row for FEB board ID according to the mac5 value
+    mac5s = list(df.mac5.unique())
+    df['feb_num'] = df['mac5'].apply(lambda x: mac5s.index(x))
+    
+    if channel >= 0:
+        single_channel_plot(df, board_id, channel)
+    else:
+        plot_32_channels(df, board_id)
 
 
 if __name__ == '__main__':
