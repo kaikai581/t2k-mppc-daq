afa4a39ddb201f5b57be9104192ca774f0ac7069
Shih-Kai Lin <kaikai581@hotmail.com>
2020-07-31 16:13:30 -0500

    Analysis work in progress...

--- FEBDAQMULTx2/data_analysis/validation/single_channel_photon_distribution.py
+++ FEBDAQMULTx2/data_analysis/validation/single_channel_photon_distribution.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import argparse
 import uproot
 
 def main():
-    pass
+    # command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_filename', type=str,
+           default='mppc_20200728.root')
+    args = parser.parse_args()
+    infpn = args.input_filename
+
 
 if __name__ == '__main__':
     main()
