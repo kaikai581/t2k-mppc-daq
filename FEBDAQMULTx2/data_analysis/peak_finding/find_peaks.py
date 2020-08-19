2a9a95735f6a08dc036b074960a122d747cf76e5
Shih-Kai Lin <kaikai5816787@gmail.com>
2020-08-19 02:28:40 -0500

    Now the script can plot all channels.

--- FEBDAQMULTx2/data_analysis/peak_finding/find_peaks.py
+++ FEBDAQMULTx2/data_analysis/peak_finding/find_peaks.py
@@ -74,6 +74,49 @@ def find_peak(infpn, feb_id, ch):
     plt.savefig(outfig_pn)
 
 
+def plot_32_channels(infpn, feb_id):
+    # retrieve the dataframe
+    try:
+        df = pd.read_hdf(infpn, key='mppc')
+    except:
+        print('Error reading file {}'.format(infpn))
+        sys.exit(-2)
+    
+    # select data of the specified board
+    df_1b = df[df['feb_num'] == feb_id]
+    bins = np.linspace(0, 4100, 821)
+
+    # get number of channels in data
+    chs = [int(col[4:-1]) for col in df.columns if 'chg' in col]
+
+    # prepare the canvases
+    _, axs = plt.subplots(8, 4, figsize=(24,16))
+
+    # loop and plot all channels
+    for ch in chs:
+        cur_ax = axs[ch//4, ch%4]
+        cur_ax.tick_params(axis='x', labelsize=7)
+        cur_ax.tick_params(axis='y', labelsize=7)
+        chvar = 'chg[{}]'.format(ch)
+        df_1b[chvar].plot.hist(bins=bins, histtype='step', ax=cur_ax)
+        cur_ax.set_xlabel('ADC')
+        cur_ax.set_ylabel('')
+        # cur_ax.set_yscale('log')
+        cur_ax.text(0.7, 0.6,'FEB {} ch {}'.format(feb_id, ch), ha='center',
+                    va='center', transform=cur_ax.transAxes, color='r')
+    
+    plt.tight_layout()
+
+    # save to file
+    infn = os.path.basename(infpn)
+    outfdname = os.path.join(os.path.dirname(__file__), infn)
+    outfdname = os.path.join(os.path.splitext(outfdname)[0], 'all_channels')
+    if not os.path.exists(outfdname):
+        os.makedirs(outfdname)
+    outfig_pn = os.path.join(outfdname, 'board{}.png'.format(feb_id))
+    print('Saving output to {}'.format(outfig_pn))
+    plt.savefig(outfig_pn)
+
 def main():
     # command line arguments
     parser = argparse.ArgumentParser()
@@ -89,12 +132,15 @@ def main():
     if board_id > 1 or board_id < 0:
         print('Board ID should be 0 or 1.')
         sys.exit(-1)
-    if channel > 31 or channel < 0:
-        print('Channel number should be between 0 and 31.')
+    if channel > 31:
+        print('Channel number should be between 0 and 31. Use a negative number to plot all channels')
         sys.exit(-1)
 
     # process a single channel
-    find_peak(infpn, board_id, channel)
+    if channel >= 0:
+        find_peak(infpn, board_id, channel)
+    else:
+        plot_32_channels(infpn, board_id)
 
 
 if __name__ == '__main__':
