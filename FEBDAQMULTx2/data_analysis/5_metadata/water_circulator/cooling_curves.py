b751e8aced68d06ef74bfc0c24c486af2a6341e1
kaikai <kaikai581@hotmail.com>
2020-10-05 23:55:43 -0500

    Now one can specify a time range to plot.

--- FEBDAQMULTx2/data_analysis/5_metadata/water_circulator/cooling_curves.py
+++ FEBDAQMULTx2/data_analysis/5_metadata/water_circulator/cooling_curves.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 import argparse
+import dateparser
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -11,14 +12,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--thermal_file', type=str, default='../../data/metadata/temperature_sensor_readings.csv')
     parser.add_argument('-w', '--water_file', type=str, default='../../data/metadata/water_circulator_readback.csv')
+    parser.add_argument('-s', '--start_datetime', type=dateparser.parse, default='2020/10/5 9:00:00', help='Wrap the date and time in quotes!')
+    parser.add_argument('-e', '--end_datetime', type=dateparser.parse, default='2020/10/5 19:00:00', help='Wrap the date and time in quotes!')
     args = parser.parse_args()
     inf_therm = args.thermal_file
     inf_water = args.water_file
+    start_datetime = args.start_datetime
+    end_datetime = args.end_datetime
 
     # load dataframe
     df_therm = pd.read_csv(inf_therm, parse_dates=['Datetime'])
+    df_therm = df_therm[(df_therm['Datetime'] >= start_datetime) & (df_therm['Datetime'] <= end_datetime)]
     df_therm['Time'] = df_therm['Datetime'].dt.strftime('%H:%M:%S')
     df_water = pd.read_csv(inf_water, parse_dates=['Datetime'])
+    df_water = df_water[(df_water['Datetime'] >= start_datetime) & (df_water['Datetime'] <= end_datetime)]
     df_water['Time'] = df_water['Datetime'].dt.strftime('%H:%M:%S')
     
     # plot readings from all sensors
@@ -29,16 +36,16 @@ if __name__ == '__main__':
     plt.plot(df_water['Time'], df_water['Internal Temperature'], '*', label='setpoint readback', fillstyle='none')
 
     # decorations
-    plt.ylim(bottom=min(list(df_water['Internal Temperature'])+list(df_therm['T0'])+list(df_therm['T1'])+list(df_therm['T2'])+list(df_therm['T3'])+list(df_therm['T4']))*.8)
+    plt.ylim(bottom=min(list(df_water['Internal Temperature'])+list(df_therm['T0'])+list(df_therm['T1'])+list(df_therm['T2'])+list(df_therm['T3'])+list(df_therm['T4']))*.7)
     plt.legend()
     plt.grid(axis='y')
     plt.xlabel('wall time')
     plt.ylabel(u'temperature (℃)')
-    plt.xticks(rotation='vertical', ticks=df_therm[df_therm.index % 10 == 0]['Time'])
+    plt.xticks(rotation='vertical', ticks=df_therm[df_therm.index % (len(df_therm)//20) == 0]['Time'])
     plt.tight_layout()
 
     # save to file
     out_dir = 'plots'
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    plt.savefig(os.path.join(out_dir, '{}.jpg'.format(df_therm['Datetime'].dt.strftime('%m-%d-%Y_%H-%M-%S')[0])))
+    plt.savefig(os.path.join(out_dir, '{}-{}.jpg'.format(df_therm['Datetime'].dt.strftime('%m-%d-%Y_%H-%M-%S').iloc[0], df_therm['Datetime'].dt.strftime('%H-%M-%S').iloc[-1])))
