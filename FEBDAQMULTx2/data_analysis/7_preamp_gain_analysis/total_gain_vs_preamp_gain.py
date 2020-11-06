961526f8d86f9cbf58dc0e540d0eab59c7502cef
Shih-Kai Lin <kaikai581@hotmail.com>
2020-11-05 19:26:11 -0600

    Clean up code and add new analysis.
    - Start to move all utility methods/classes to a dedicated folder.
    
    - Add a script to analyze preamp gain scan data.

---
+++ FEBDAQMULTx2/data_analysis/7_preamp_gain_analysis/total_gain_vs_preamp_gain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os, sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../utilities'))
+
+import argparse
+import common_tools
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_files', type=str, nargs='*')
+    parser.add_argument('-b', '--board', type=int, default=0)
+    parser.add_argument('-c', '--channel', type=int, default=24)
+    args = parser.parse_args()
+    infpns = args.input_files
+    
+    # result containers
+    mppc_lines = [common_tools.MPPCLine(infpn, args.board, args.channel, prom=50, verbose=True) for infpn in infpns]
+    mppc_lines[0].show_spectrum()
+    mppc_lines[0].show_gain_vs_pe()
+    print(mppc_lines[0].preamp_gain)
