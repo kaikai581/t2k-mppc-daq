b2e52cc770979c62fc86453ded14622ba09a97c3
Shih-Kai Lin <kaikai581@hotmail.com>
2021-06-18 16:42:45 -0500

    Add a folder to analyze data.
    From the original DAQ.

--- FEBDAQMULTx2/data_analysis/10_caen_daq_data/plot_totalgain_vs_bias_for_breakdown.py
+++ FEBDAQMULTx2/data_analysis/10_caen_daq_data/plot_totalgain_vs_bias_for_breakdown.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
-class old_daq_data:
-    def __init_(self):
-        pass
+import uproot
+
+class DAQFile:
+    def __init__(self, infpn):
+        '''
+        Constructor in charge of loading a data file.
+        '''
+        tr_mppc = uproot.open(infpn)['mppc']
+        self.df = tr_mppc.arrays(library='pd')
+
+        # store the input file pathname
+        self.infpn = infpn
+        # store the FEB SN
+        self.feb_sn = None
+
+if __name__ == '__main__':
+    my_file = DAQFile('../data//root/led/20210615_200000_new_ch24_thr205_gain56_temp22.75_trig24_feb12808_olddaq/mppc_56.5V.root')
+    print(my_file.df)
